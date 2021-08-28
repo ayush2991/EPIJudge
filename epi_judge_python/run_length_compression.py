a3fb862c15a536e4d7e9a9adf80109b36a07356b
Aayush Agarwal <ayush2991@gmail.com>
2021-08-28 19:48:41 -0400

    Solve 6.12

--- epi_judge_python/run_length_compression.py
+++ epi_judge_python/run_length_compression.py
@@ -4,12 +4,34 @@ from test_framework.test_failure import TestFailure
 
 def decoding(s: str) -> str:
     # TODO - you fill in here.
-    return ''
+    result = []
+    i = 0
+    while i < len(s):
+        count = ''
+        while s[i].isdigit():
+            count += s[i]
+            i += 1
+        letter = s[i]
+        i += 1
+        count = int(count)
+        for _ in range(count):
+            result.append(letter)
+    return ''.join(result)
 
 
 def encoding(s: str) -> str:
     # TODO - you fill in here.
-    return ''
+    result = []
+    i = 0
+    while i < len(s):
+        letter = s[i]
+        count = 0
+        while i < len(s) and s[i] == letter:
+            count += 1
+            i += 1
+        result.append(str(count))
+        result.append(letter)
+    return ''.join(result)
 
 
 def rle_tester(encoded, decoded):
