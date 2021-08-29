30e41f5b8b91a0f931527f3bcb52cca6864f624e
Aayush Agarwal <ayush2991@gmail.com>
2021-08-29 19:42:31 -0400

    Solve 9.1

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -4,7 +4,15 @@ from test_framework import generic_test
 
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
     # TODO - you fill in here.
-    return True
+    def helper(tree):
+        if not tree:
+            return 0
+        left_height = helper(tree.left)
+        right_height = helper(tree.right)
+        if abs(left_height - right_height) > 1 or left_height == -1 or right_height == -1:
+            return -1
+        return max(left_height, right_height) + 1
+    return True if helper(tree) != -1 else False
 
 
 if __name__ == '__main__':
