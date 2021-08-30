12e68fea8e9528c144e4eed0a196ed3cf41333fa
Aayush Agarwal <ayush2991@gmail.com>
2021-08-30 12:46:14 -0400

    Solve 8.2

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -3,7 +3,29 @@ from test_framework import generic_test
 
 def evaluate(expression: str) -> int:
     # TODO - you fill in here.
-    return 0
+    def is_operator(op):
+        return op == '+' or op == '-' or op == '*' or op == '/'
+
+    def apply(a, b, op):
+        if op == '+':
+            return a + b
+        elif op == '-':
+            return a - b
+        elif op == '*':
+            return a * b
+        else:
+            return a // b
+
+    ops = expression.split(',')
+    stack = []
+    for op in ops:
+        if not is_operator(op):
+            stack.append(int(op))
+        else:
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(apply(a, b, op))
+    return stack[-1]
 
 
 if __name__ == '__main__':
