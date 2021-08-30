f4491570930f817c6f6a46f06664e78197797dc4
Aayush Agarwal <ayush2991@gmail.com>
2021-08-30 16:22:37 -0400

    Solve 16.2

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -3,7 +3,15 @@ from test_framework import generic_test
 
 def levenshtein_distance(A: str, B: str) -> int:
     # TODO - you fill in here.
-    return 0
+    m = [[0]*(len(B)+1) for _ in range(len(A) + 1)]
+    for i in range(len(B) + 1):
+        m[0][i] = i
+    for i in range(len(A) + 1):
+        m[i][0] = i
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            m[i][j] = min(m[i][j-1] + 1, m[i-1][j] + 1, m[i-1][j-1] + (0 if A[i-1] == B[j-1] else 1))
+    return m[-1][-1]
 
 
 if __name__ == '__main__':
