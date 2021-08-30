6880a9c8adc41cb5f4d2045a0940672a10e23f81
Aayush Agarwal <ayush2991@gmail.com>
2021-08-29 20:41:29 -0400

    Solve 9.5

--- epi_judge_python/sum_root_to_leaf.py
+++ epi_judge_python/sum_root_to_leaf.py
@@ -4,7 +4,14 @@ from test_framework import generic_test
 
 def sum_root_to_leaf(tree: BinaryTreeNode) -> int:
     # TODO - you fill in here.
-    return 0
+    def helper(tree, s):
+        if not tree: 
+            return 0
+        value = 2 * s + tree.data
+        if not tree.left and not tree.right:
+            return value
+        return helper(tree.left, value) + helper(tree.right, value)
+    return helper(tree, 0)
 
 
 if __name__ == '__main__':
